754384e8ecfce4a4a21abc5a1ea4407f23311726
Michael <michael.kononenko101@gmail.com>
2022-09-24 23:22:42 +0300

    add second type of block

--- js/simulation-logic.js
+++ js/simulation-logic.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
 
 const canva = document.querySelector(".canvas-area");
+let id = 0;
 //find mouse coordinates
 let xPosition = 0;
 let yPosition = 0;
@@ -11,17 +12,43 @@ canva.addEventListener(
     yPosition = evt.clientY;
   })
 );
-let id = 0;
-canva.addEventListener("click", doMagick);
-function doMagick(event) {
+//click handling inside canvas
+canva.addEventListener("click", () => renderBox("simple"));
+canva.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  renderBox("high-level");
+});
+
+//buttons click handling
+body.addEventListener("click", buttonEvent);
+function buttonEvent(event) {
+  const target = event.target.id;
+  console.log(target);
+  switch (target) {
+    case "simple-cube":
+      renderBox("simple", true);
+      break;
+    case "high-level-cube":
+      renderBox("high-level", true);
+      break;
+  }
+}
+
+//rendering new box
+function renderBox(level, center) {
+  const color = level === "simple" ? "pink" : "purple";
   const headerHeight = document.querySelector(".header").offsetHeight;
   const displayWidth = body.offsetWidth;
   const canvaWidth = canva.offsetWidth;
+  if (center) {
+    xPosition = displayWidth / 2 + 7;
+    yPosition = headerHeight + 30 + 150;
+  }
   id += 1;
   const blockY = yPosition - headerHeight - 45;
   const blockX = xPosition - displayWidth / 2 + canvaWidth / 2 - 25;
   canva.insertAdjacentHTML(
     "beforeend",
-    `<div id="${id}" class="simple-block" style="background-color: pink; top: ${blockY}px; left:${blockX}px"></div>`
+    `<div id="${id}" class="simple-block" style="background-color:${color}; top: ${blockY}px; left:${blockX}px"></div>`
   );
 }
